3f38c8e35de3dc0007dc2bccf73ef52c28f2b2e0
LUCAS EVANGELISTA <63316648+Dasnevesevangelista@users.noreply.github.com>
2022-08-21 21:23:57 -0300

    atualização

--- calculadora/script.js
+++ calculadora/script.js
@@ -1,22 +1,4 @@
-function insert(num) {
-    var numero = document.getElementById('resultado').innerHTML
-    document.getElementById('resultado').innerHTML =  parseInt(numero + num);
-}
-function clean(){
-    document.getElementById('resultado').innerHTML = '';
-}
-function back(){
-    var resultado = document.getElementById('resultado').innerHTML;
-    document.getElementById('resultado').innerHTML = resultado.substring(0, resultado.length - 1)
-}
-function calcular(){
-    var resultado = document.getElementById('resultado').innerHTML;
-    if(resultado)
-    {
-        document.getElementById('resultado').innerHTML = eval(resultado)
-    }
-    else
-    {
-        document.getElementById('resultado').innerHTML = 'Sem Resultado'
-    }
+function insert(num){
+    let numero1 = document.getElementById('calculo').innerHTML 
+       document.getElementById('calculo').innerHTML = numero1 + num
 }
